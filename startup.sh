3aedc6e51d06f5f5d6d93f00f13d691317d48d08
svobodam <matej.svoboda@outlook.com>
2017-08-19 20:38:46 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,41 +1,45 @@
 #!/bin/bash
-# Author: Matej Svoboda
 # Startup script to run scripts in correct sequence
 
-LSTM-ATTN = runScripts/trainLSTM-ATTN.py
-LSTM-NO-ATTN = runScripts/trainLSTM-noATTN.py
-GRU-ATTN = runScripts/trainGRU-ATTN.py
-GRU-NO-ATTN = runScripts/trainGRU-noATTN.py
+LSTMATTN=$HOME/deep-Learning-Text-Summariser/runScripts/trainLSTM-ATTN.py
+LSTMNOATTN=$HOME/deep-Learning-Text-Summariser/runScripts/trainLSTM-noATTN.py
+LSTMBIDATTN=$HOME/deep-Learning-Text-Summariser/runScripts/trainLSTM-Bidirectional-ATTN.py
+LSTMBIDNOATTN=$HOME/deep-Learning-Text-Summariser/runScripts/trainLSTM-Bidirectional-noATTN.py
 
 # Run Explorartion R Script
-Rscript PreProcessingScript/source.r
-
+echo Starting Dataset Preprocessing...
+echo Output from Dataset preprocessing...
+#Rscript PreProcessingScript/source.r
+echo Dataset preprocessing completed...
+echo ...
+echo Starting deep-learning process...
 # Running deep-learning
 
-echo('Please select deep-learning model:')
-echo('...')
-echo('[1] LSTM-Attention Enabled')
-echo('[2] LSTM-Attention Disabled')
-echo('[3] GRU-Attention Enabled')
-echo('[4] GRU-Attention Disabled')
-echo('Select your option:')
+echo Please select deep-learning model:
+echo ...
+echo [1] LSTM-Attention Enabled
+echo [2] LSTM-Attention Disabled
+echo [3] LSTM-Bidirectional-Attention Enabled
+echo [4] LSTM-Bidirectional-Attention Disabled
+echo Select your option:
 read option
 
-if [$option == 1]; then
-	python $LSTM-ATTN
+if [ $option ==  1 ]; then
+	python $LSTMATTN
 	exit 1
-	elif [$option == 2]; then
-	python $LSTM-NO-ATTN
+	elif [ $option == 2 ]; then
+	python $LSTMNOATTN
 	exit 1
-	elif [$option == 3]; then
-	python $GRU-ATTN
+	elif [ $option == 3 ]; then
+	python $LSTMBIDATTN
 	exit 1
-	elif [$option == 4]; then
-	python $GRU-NO-ATTN
+	elif [ $option == 4 ]; then
+	python $LSTMBIDNOATTN
 	exit 1
 else
-	echo('You have selected non-existing option...')
+	echo You have selected non-existing option...
 	exit 1
 	
 fi
+
 
