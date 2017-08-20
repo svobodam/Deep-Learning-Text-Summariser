bea08f3cb846fbfdaf84b4cbed376cae02554413
svobodam <matej.svoboda@outlook.com>
2017-08-20 16:20:18 +0100

    Update extracterDB.r

--- PreProcessingScript/extracterDB.r
+++ PreProcessingScript/extracterDB.r
@@ -1,19 +1,46 @@
-# Author: Matej Svoboda
 # Data Frame exploration.
-# Extract data from Documents and summaries into rewsum1 to table in DB
+# Extract data from Documents and document summaries into new subset and prepare them for extarction to table in DB.
+
+# Extract Documents from df > dfData
+counter = 1
+while(counter <= 25){
+  for (m in doc) {
+    if ((exists("dfData"))==TRUE) {
+      dfEdit=data.frame(with(df, paste0(df[[m]])), stringsAsFactors=FALSE)
+      colnames(dfEdit)="Documents"
+      dfData=rbind(dfData,dfEdit)
+    } else {
+      dfData=data.frame(with(df, paste0(df[[m]])),stringsAsFactors=FALSE)
+      colnames(dfData)="Documents"
+    }
+    counter = counter + 1
+    
+  }
+}
 
 
-# SUBSET CREATION
+# Extract Document names from df > dfDataNames
 
-# Create Subset and merge all columns into one column called "Documents". Column "Summaries" = summary of document
-dfData = with(df, paste0(Document1, Document2, Document3, Document4, Document5, Document6, Document7, Document8, Document9, Document10, Document11, Document12, Document13, Document14, Document15, Document16, Document17, Document18, Document19, Document20, Document21, Document21, Document22, Document23, Document24, Document25))
-#
-#
-dfEdited = data.frame(dfData)
-# Create new column named "Documents"
-colnames(dfEdited) ="Documents"
-dfEdited$Summaries = df$Sum1
+docNames = c('DocumentName1', 'DocumentName2', 'DocumentName3', 'DocumentName4', 'DocumentName5', 'DocumentName6', 'DocumentName7', 'DocumentName8', 'DocumentName9', 'DocumentName10', 'DocumentName11', 'DocumentName12', 'DocumentName13', 'Document14', 'DocumentName15', 'DocumentName16', 'DocumentName17', 'DocumentName18', 'DocumentName19', 'DocumentName20', 'DocumentName21', 'DocumentName22', 'DocumentName23', 'DocumentName24', 'DocumentName25')
+counter = 1
+while(counter <= 25){
+  for (w in docNames) {
+    if ((exists("dfDataNames"))==TRUE) {
+      dfEditNames=data.frame(with(df, paste0(df[[w]])), stringsAsFactors=FALSE)
+      colnames(dfEditNames)="DocSum"
+      dfDataNames=rbind(dfDataNames,dfEditNames)
+    } else {
+      dfDataNames=data.frame(with(df, paste0(df[[w]])),stringsAsFactors=FALSE)
+      colnames(dfDataNames)="DocSum"
+    }
+    counter = counter + 1
+    
+  }
+}
+
+#Merge data frames together
+dfData$DocSum = dfDataNames$DocSum
 
 # Status echo
-cat('Extracting completed.')
+cat('Extracting completed. Moving to another stage...')
 
